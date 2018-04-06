7ab09d2519c6a7a3fd9151c9d5a9d8b3447d8108
bhsuarez <bhsuarez@me.com>
2018-04-05 21:46:04 -0500

    Draft of Unique.java with comments

--- Unique/src/Unique.java
+++ Unique/src/Unique.java
@@ -1,2 +1,46 @@
+import java.util.Arrays;
+
 public class Unique {
+
+    /*
+      Boolean method to determine if array elements are unique
+     */
+    private static boolean isUnique(String[] s){
+
+        // String variables
+        String currentString;
+        String nextString;
+        int j = 0;
+
+        // Loop to iterate array to print array elements
+        for(int i = 0; i < s.length; i++){
+
+            // Print the current array element
+            currentString = Arrays.asList(s).get(i);
+            System.out.println(i + " " + currentString);
+        }
+        return false;
+    }
+
+    // Returns sample unique array
+    private static String[] sampleArray(){
+        return new String[] {"hello", "world", "my", "name", "is", "jonas"};
+    }
+
+    // Returns sample same array
+    private static String[] sampleSameArray(){
+        return new String[] {"hello", "hello", "hello", "hello", "hello", "hello"};
+    }
+
+    /* Main method
+
+     */
+    public static void main(String[] args){
+
+        // Prints true or false
+        System.out.println(isUnique(sampleArray())+"\n");
+        System.out.println(isUnique(sampleSameArray()));
+
+    }
+
 }
