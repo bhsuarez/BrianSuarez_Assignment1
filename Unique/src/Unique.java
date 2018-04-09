e12c61ab4f1094001a8cf90a891422c40108c7f0
bhsuarez <bhsuarez@me.com>
2018-04-09 18:00:51 -0500

    Changed for loop to foreach, cleaned up, added comments, corrected output

--- Unique/src/Unique.java
+++ Unique/src/Unique.java
@@ -1,57 +1,59 @@
-import java.util.Arrays;
-
 public class Unique {
 
     /*
-      Boolean method to determine if array elements are unique
+       Private boolean method with String[] argument to determine if array elements are unique
      */
     private static boolean isUnique(String[] s){
 
-        // String variables
-        String initialString = s[0];
-        String currentString;
-        boolean isUnique = true;
+            // Define initialString to set initial array element
+            String initialString = s[0];
 
-        // Loop to iterate array to print array elements
-        for(int i = 0; i < s.length -1; i++){
+            // Define currentString for comparing elements to initialString
+            String currentString;
 
-            // Sets currentString String variable
-            currentString = s[i];
+            // Boolean variable to set if array has unique values
+            boolean isUnique = true;
 
-            // Sets nextString String variable
-            //nextString = Arrays.asList(s).get(i+1);
+            // Loop to iterate array to print array elements
+        for (String value : s) {
 
-            System.out.println(i + " " + currentString+" "+initialString+" "+isUnique);
+            // Sets currentString String variable as the first element of array
+            currentString = value;
 
-            // If statement to determine if currentString equals nextString
-            if(!initialString.equals(currentString)){
+            // If statement to determine if currentString does not equal nextString
+            if (!initialString.equals(currentString)) {
+
+                // Set isUnique to false
                 isUnique = false;
             }
 
-
+            // Debugging
+            // System.out.println(i+" "+initialString+" "+currentString+" "+isUnique);
         }
-        return isUnique;
+
+            // Return isUnique boolean
+            return isUnique;
     }
 
-    // Returns sample unique array
-    private static String[] sampleArray(){
+    // Private static String[] which returns sample array with unique values
+    private static String[] sampleArray() {
         return new String[] {"hello", "world", "my", "name", "is", "jonas"};
     }
 
-    // Returns sample same array
+    // Private static String[] which returns sample same with same values
     private static String[] sampleSameArray(){
-        return new String[] {"hello", "hello", "hello", "hello", "hello", "hello"};
+        return new String[] {"hello", "hello", "hello", "hello", "hello"};
     }
 
-    /* Main method
+
+    /* Main method for output
 
      */
     public static void main(String[] args){
 
-        // Prints true or false
-        System.out.println(isUnique(sampleArray())+"\n");
-        System.out.println(isUnique(sampleSameArray()));
+        //
+        System.out.println("The 'sampleArray' array has all unique values: "+isUnique(sampleArray()));
+        System.out.println("The 'sampleSameArray' has all unique values: "+isUnique(sampleSameArray()));
 
     }
-
 }
